322801f056ac30ed6bd82aa499fc88f4c7a563ff
fredMilloh <fredmilloh@icloud.com>
2022-03-21 17:09:16 +0100

    Initial Commit

---
+++ ProductRecall/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProductRecall
+//
+//  Created by fred on 21/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProductRecall/ProductRecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductRecallApp.swift
+//  ProductRecall
+//
+//  Created by fred on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProductRecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
