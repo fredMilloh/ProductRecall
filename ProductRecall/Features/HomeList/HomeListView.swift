c7372d9d60fbd438bca555174aee900698947cb7
fredMilloh <fredmilloh@icloud.com>
2022-03-24 18:42:49 +0100

    added search by suggestions item or category

--- ProductRecall/Features/HomeList/HomeListView.swift
+++ ProductRecall/Features/HomeList/HomeListView.swift
@@ -24,7 +24,39 @@ struct HomeListView: View {
     }
     var body: some View {
         NavigationView {
-            VStack {
+            List(filteredRecords) { record in
+                    NavigationLink {
+                        ProductDetail(
+                            recordViewModel: RecordViewModel(record: record)
+                        )
+                    } label: {
+                        let viewModel = RecordViewModel(record: record)
+                        RecordRow(recordViewModel: viewModel)
+                    }
+                    .onAppear(perform: {
+                        if !self.recordsFeed.endOfList {
+                            if self.recordsFeed.shouldLoadMore(recordItem: record) {
+                                self.recordsFeed.get()
+                            }
+                        }
+                    })
+                    .alert(isPresented: $recordsFeed.endOfList) {
+                        Alert(title: Text("Oups"),
+                              message: Text("An error Occurred"),
+                              dismissButton: .default(Text("ok")))
+                    }
+            }
+            .navigationTitle("Rappels Produits")
+            .refreshable {
+                recordsFeed.get()
+            }
+        }
+        .searchable(
+            text: $viewModel.searchText,
+            placement: .navigationBarDrawer(displayMode: .always),
+            prompt: "Rechercher un produit, une marque, ...",
+            suggestions: {
+                
                 if viewModel.isSearchEnabled {
                     Button {
                         recordsFeed.endPoint = ProductsEndpoint.whereSearchIs(string: viewModel.searchText)
@@ -32,40 +64,30 @@ struct HomeListView: View {
                         recordsFeed.productsRecall.removeAll()
                         recordsFeed.get()
                     } label: {
-                        Text("Lancer une recherche dans l'historique")
+                        HStack {
+                            Text("Lancer une recherche avec **\(viewModel.searchText)**")
+                            Image(systemName: "magnifyingglass")
+                        }
                     }
-                    .buttonStyle(.bordered)
                     .foregroundColor(.blue)
                 }
-                List {
-                    ForEach(filteredRecords) { record in
-                        NavigationLink {
-                            ProductDetail(recordViewModel: RecordViewModel(record: record))
+                
+                Section(header: Text("Catégories").bold()) {
+                    ForEach(Category.categories, id: \.id) { category in
+                        Button { [self] in
+                            recordsFeed.endPoint = ProductsEndpoint.whereCategoryIs(search: category.name)
+                            recordsFeed.pageStatus = PageStatus.ready(nextOffset: 0)
+                            recordsFeed.productsRecall.removeAll()
+                            recordsFeed.get()
                         } label: {
-                            let viewModel = RecordViewModel(record: record)
-                            RecordRow(recordViewModel: viewModel)
-                        }
-                        .onAppear(perform: {
-                            if !self.recordsFeed.endOfList {
-                                if self.recordsFeed.shouldLoadMore(recordItem: record) {
-                                    self.recordsFeed.get()
-                                }
+                            HStack {
+                                Image(systemName: "magnifyingglass")
+                                Text(category.name)
                             }
-                        })
-                        .alert(isPresented: $recordsFeed.endOfList) {
-                            Alert(title: Text("Oups"), message: Text("An error Occurred"), dismissButton: .default(Text("ok")))
                         }
                     }
                 }
-                .listStyle(PlainListStyle())
-            }
-            .navigationTitle("Rappels Produits")
-        }
-        .searchable(
-            text: $viewModel.searchText,
-            placement: .navigationBarDrawer(displayMode: .always),
-            prompt: "Rechercher un produit, une marque, ..."
-        )
+        })
     }
 }
 
